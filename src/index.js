0e20fde1969284c97b20fcb2c68f4992ac5afe46
Ajay KC <meajay64@gmail.com>
2022-08-01 23:16:25 +0900

    Import nav and add to content

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-// import { imgCollection } from "../modules/imgObject";
-import "./assets/style.css";
+import "./assets/stylesheets/normalize.css";
+import "./assets/stylesheets/styles.css";
+
+// Import Modules
+
+import addNav from "./modules/nav";
+
+let contentDiv = document.querySelector('.content');
+
+contentDiv.appendChild(addNav());
