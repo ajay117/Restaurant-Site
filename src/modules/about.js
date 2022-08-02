59e38ae4cd44076c59ade3981128da686c4b2731
Ajay KC <meajay64@gmail.com>
2022-08-02 15:51:45 +0900

    Add about info

--- src/modules/about.js
+++ src/modules/about.js
@@ -1,6 +1,14 @@
+import addNav from "./nav";
+
 export default function addAbout() {
+  let contentDiv = document.querySelector(".content");
+  
+  if(contentDiv.innerHTML) {
+    contentDiv.innerHTML = "";
+  }
   let div = document.createElement("div");
   div.textContent = "About Page";
 
-  return div;
+  contentDiv.appendChild(addNav());
+  contentDiv.appendChild(div);
 }
