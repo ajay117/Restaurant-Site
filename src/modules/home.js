4c29f4f3820c73060430dcca097fb67d773cc24f
Ajay KC <meajay64@gmail.com>
2022-08-02 22:41:55 +0900

    Add text content to page

--- src/modules/home.js
+++ src/modules/home.js
@@ -12,15 +12,15 @@ export default function addHome() {
   let para = document.createElement("p");
   let btn = document.createElement("button");
 
-  heading.textContent = "Lorem ipsum dolor sit amet";
-  para.textContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  heading.textContent = "Aj's Kitchen";
+  para.textContent = "Hangout Experience with great atmosphere";
 
   btn.textContent = "Explore";
+  btn.classList.add('btn-info');
 
-  btn.addEventListener('click', () => {
+  btn.addEventListener("click", () => {
     addMenu();
-  })
+  });
 
   introDiv.appendChild(heading);
   introDiv.appendChild(para);
@@ -29,6 +29,10 @@ export default function addHome() {
   introDiv.classList.add("intro");
   introDiv.classList.add("padded-container");
 
+  // contentDiv.innerHTML =
+  //   '<div class="video-container"><iframesrc="https://www.youtube.com/embed/f7MHvMsDnPM?controls=0&autoplay=1&mute=1&playlist=f7MHvMsDnPM&loop=1"></iframe></div><div class="content"></div>';
+
+  // contentDiv.appendChild(addVideoBackground());
   contentDiv.appendChild(addNav());
   contentDiv.appendChild(introDiv);
 }
