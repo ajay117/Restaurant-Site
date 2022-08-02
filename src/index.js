905ede2e6932cac3745276a1087506476f212984
Ajay KC <meajay64@gmail.com>
2022-08-02 22:44:31 +0900

    Remove space

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "./assets/stylesheets/styles.css";
 import addHome from "./modules/home";
 
 addHome();
-
 window.addEventListener("DOMContentLoaded", () => {
     let intro = document.querySelector('.intro');
     intro.classList.add("animate");
