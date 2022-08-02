87c3a937338c6fc8ef4a21537af271b91f64ba93
Ajay KC <meajay64@gmail.com>
2022-08-02 16:57:07 +0900

    Transition intro content on load

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import "./assets/stylesheets/normalize.css";
 import "./assets/stylesheets/styles.css";
-
-import addNav from "./modules/nav";
-import introContainer from "./modules/intro";
 import addHome from "./modules/home";
 
-// Import Modules
-
-// let contentDiv = document.querySelector('.content');
-
-// contentDiv.appendChild(addNav());
-// contentDiv.appendChild(introContainer());
-
 addHome();
+
+window.addEventListener("DOMContentLoaded", () => {
+    let intro = document.querySelector('.intro');
+    intro.classList.add("animate");
+})
