3036a5a87716bb92d2f251501dade4b8d7f0969b
Ajay KC <meajay64@gmail.com>
2022-08-02 15:50:33 +0900

    Create and  add dishes object

---
+++ src/menuObj/appetizer.js
@@ -0,0 +1,38 @@
+const appetizer = [
+  {
+    name: "Aloo Tikki",
+    src: "https://img.buzzfeed.com/buzzfeed-static/static/2014-09/23/11/enhanced/webdr10/enhanced-21909-1411487035-20.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto",
+    description:
+      "Think of aloo tikki as delicious Indian potato fritters. These appetizers or snacks are made with mashed potatoes and a variety of spices, formed into patties, then fried in oil.",
+  },
+  {
+    name: "Mango Chutney",
+    src: "https://img.buzzfeed.com/buzzfeed-static/static/2014-09/23/12/enhanced/webdr01/longform-original-5715-1411488852-29.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+    description:
+      "This quintessential Indian condiment is used to top many Indian appetizers and is made with mangoes, vinegar, sugar, and spices. Mango chutney is probably the most recognizable chutney.",
+  },
+  {
+    name: "Samosa",
+    src: "https://img.buzzfeed.com/buzzfeed-static/static/2014-09/23/11/enhanced/webdr02/longform-original-11540-1411487102-19.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+    description:
+      "Think of samosas as being an Indian version of empanadas. These are a favorite appetizer made with with a flaky pastry enveloping a hearty filling of potatoes and vegetables. They're folded into triangles, fried, and served with chutneys.",
+  },
+  {
+    name: "Raita",
+    src: "https://img.buzzfeed.com/buzzfeed-static/static/2014-09/23/11/enhanced/webdr09/longform-original-11349-1411487122-3.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+    description:
+      "Raita is your best friend if you're having Indian food for the first time. Not exactly sure how spicy your dish is going to be? Think you might not be able to handle it? Have no fear. Raita's got your back. This cooling cucumber-mint yogurt sauce will dial down the heat if things get too hot.",
+  },
+  {
+    name: "Momos",
+    src: "https://img.buzzfeed.com/buzzfeed-static/static/2014-09/25/8/enhanced/webdr11/enhanced-321-1411646686-1.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+    description:
+      "Momos are simply  dumplings. The spices used in the filling and sauce are more characteristic of Nepalese cuisine than Chinese cuisine, but like dumplings, momos are steamed or fried and come with a variety of vegetable and meat fillings.",
+  },
+  {
+    name: "Pakore/Bhaji",
+    src: "https://img.buzzfeed.com/buzzfeed-static/static/2014-09/23/13/enhanced/webdr05/longform-original-22620-1411492130-3.jpg?downsize=600:*&output-format=auto&output-quality=auto",
+    description:
+      "Pakora and bhaji are two terms for vegetables that are battered and deep fried, kind of like tempura. Onion is one of the most popular varieties of pakora and bhaji, but other vegetables are prepared the same way. That's a pretty convincing way to get your 3-5 a day.",
+  },
+];
