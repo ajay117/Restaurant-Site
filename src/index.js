c459a4d89db25d892b9d6554ba41540d8d660b81
Ajay KC <meajay64@gmail.com>
2022-08-02 14:05:19 +0900

    Use clear method to add elements

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import "./assets/stylesheets/styles.css";
 
 import addNav from "./modules/nav";
 import introContainer from "./modules/intro";
+import addHome from "./modules/home";
 
 // Import Modules
 
+// let contentDiv = document.querySelector('.content');
 
-let contentDiv = document.querySelector('.content');
+// contentDiv.appendChild(addNav());
+// contentDiv.appendChild(introContainer());
 
-contentDiv.appendChild(addNav());
-contentDiv.appendChild(introContainer());
+addHome();
