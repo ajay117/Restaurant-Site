ddb97d2cf624d929e08fd35a5d4e776c72a46f60
Ajay KC <meajay64@gmail.com>
2022-08-06 14:09:38 +0900

    Add comment

--- src/utility/containsObject.js
+++ src/utility/containsObject.js
@@ -1,10 +1,11 @@
+//Check if an object exists in an array..
 export default function containsObject(obj, list) {
-    var i;
-    for (i = 0; i < list.length; i++) {
-        if (list[i] === obj) {
-            return true;
-        }
+  var i;
+  for (i = 0; i < list.length; i++) {
+    if (list[i] === obj) {
+      return true;
     }
+  }
 
-    return false;
+  return false;
 }
